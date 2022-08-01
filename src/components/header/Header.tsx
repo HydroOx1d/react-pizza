7177ff7846f4d8d8de545de40b20929bca32541d
HydroOx1d <n-esenov44@mail.ru>
2022-08-01 21:18:12 +0600

    update: header is finished

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import { Container, Flex } from '../uikit';
 import Logo from '../../assets/header/logo.png'
+import Basket from '../../assets/header/basket.svg'
 
 const HeaderSC = styled.header`
   padding: 50px 0px 50px 0px;
@@ -35,22 +36,61 @@ const HeaderLogoSubtitle = styled.div`
   color: #7b7b7b;
 `
 
+const HeaderBasket = styled(Flex)`
+  cursor: pointer;
+  background: #fe5f1e;
+  border-radius: 30px;
+  color: #ffffff;
+  padding: 13px 23px;
+
+  font-weight: 700;
+  font-size: 16px;
+  line-height: calc(20 / 16 * 100%);
+`;
+
+const HeaderBasketSum = styled.div`
+  border-right: 1px solid #ffffff40;
+  padding: 0px 13px 0px 0px;
+  margin: 0px 13px 0px 0px;
+`;
+
+const HeaderBasketCount = styled(Flex)`
+  
+`
+
+const HeaderBasketIcon = styled.img`
+  margin: 0px 8px 0px 0px;
+`
+
+const HeaderBasketNum = styled.div`
+  
+`
+
 const Header = () => {
   return (
     <HeaderSC>
       <Container>
-        <Flex align='center'>
-          <HeaderLogoSC align='center'>
-            <HeaderLogoImage src={Logo} alt="logo"/>
+        <Flex align="center" justify='space-between'>
+          <HeaderLogoSC align="center">
+            <HeaderLogoImage src={Logo} alt="logo" />
             <HeaderLogoContent>
               <HeaderLogoTitle>REACT PIZZA</HeaderLogoTitle>
-              <HeaderLogoSubtitle>самая вкусная пицца во вселенной</HeaderLogoSubtitle>
+              <HeaderLogoSubtitle>
+                самая вкусная пицца во вселенной
+              </HeaderLogoSubtitle>
             </HeaderLogoContent>
           </HeaderLogoSC>
+          <HeaderBasket align='center'>
+            <HeaderBasketSum>520 ₽</HeaderBasketSum>
+            <HeaderBasketCount align='center'>
+              <HeaderBasketIcon src={Basket} />
+              <HeaderBasketNum>3</HeaderBasketNum>
+            </HeaderBasketCount>
+          </HeaderBasket>
         </Flex>
       </Container>
     </HeaderSC>
-  )
+  );
 }
 
 export default Header
