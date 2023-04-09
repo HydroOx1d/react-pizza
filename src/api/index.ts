8fb321eba868f2bf9be682372eff38c91567dfe5
HydroOx1d <n-esenov44@mail.ru>
2023-04-09 13:04:01 +0600

    update: extra lines have been removed in api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,6 @@ import { PizzasType } from '../types/index';
 
 export const pizzasRequests = {
   getPizzas: async (sortBy: string | null, order: string | null) => {
-    return await axios.get<PizzasType[]>(`https://6303917a761a3bce77da10ff.mockapi.io/api/pizzas?sortBy=${sortBy !== null && sortBy}&order=${order !== null && order}`);
+    return await axios.get<PizzasType[]>(`https://6303917a761a3bce77da10ff.mockapi.io/api/pizzas?sortBy=${sortBy}&order=${order}`);
   }
 }
