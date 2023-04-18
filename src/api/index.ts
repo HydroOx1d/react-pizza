59c48e64c817b4132476d37191a079e720b67c6c
HydroOx1d <n-esenov44@mail.ru>
2023-04-18 21:11:13 +0600

    update: modify api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,9 @@ import { PizzasType } from '../types/index';
 
 export const pizzasRequests = {
   getPizzas: async (sortBy: string | null, order: string | null, category: string | null) => {
-    return await axios.get<PizzasType[]>(`https://6303917a761a3bce77da10ff.mockapi.io/api/pizzas?sortBy=${sortBy}&order=${order}&category=${category}`);
+    const sortByQuery = sortBy && `sortBy=${sortBy}`
+    const orderQuery = order && `order=${order}`
+    const categoryQuery = category && `category=${category}`
+    return await axios.get<PizzasType[]>(`https://6303917a761a3bce77da10ff.mockapi.io/api/pizzas?${sortByQuery}&${orderQuery}&${categoryQuery}`);
   }
 }
